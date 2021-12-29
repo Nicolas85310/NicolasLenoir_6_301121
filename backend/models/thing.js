eb7fa37e1c32123bbb270aa4acb4db8295ad8bff
Lenoir Nicolas <nicolas.lenoir52@sfr.fr>
2021-12-29 19:44:56 +0100

    Add gitignore + thing

--- backend/models/thing.js
+++ backend/models/thing.js
@@ -10,8 +10,8 @@ const thingSchema = mongoose.Schema({
   heat: { type: Number, required: true },
   likes: { type: Number, required: true },
   dislikes: { type: Number, required: true },
-  usersLiked: { type: String, required: true },
-  usersDisliked: { type: String, required: true },
+  usersLiked: { type: Array, required: true },
+  usersDisliked: { type: Array, required: true },
 });
 
 module.exports = mongoose.model('Thing', thingSchema);
