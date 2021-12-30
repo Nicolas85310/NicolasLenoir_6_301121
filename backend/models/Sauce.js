8ca260bd90b2ac35046d41c5222d425eab2c530c
Lenoir Nicolas <nicolas.lenoir52@sfr.fr>
2021-12-30 16:40:35 +0100

    true at false

--- backend/models/Sauce.js
+++ backend/models/Sauce.js
@@ -8,10 +8,10 @@ const sauceSchema = mongoose.Schema({
   mainPepper: { type: String, required: true },
   imageUrl: { type: String, required: true },
   heat: { type: Number, required: true },
-  likes: { type: Number, required: true },
-  dislikes: { type: Number, required: true },
-  usersLiked: { type: Array, required: true },
-  usersDisliked: { type: Array, required: true },
+  likes: { type: Number, required: false },
+  dislikes: { type: Number, required: false },
+  usersLiked: { type: Array, required: false },
+  usersDisliked: { type: Array, required: false },
 });
 
 module.exports = mongoose.model('Sauce', sauceSchema);
