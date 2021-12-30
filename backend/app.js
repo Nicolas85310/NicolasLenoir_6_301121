6e3fee713a744a8a723e3788b8df2af0863d6ff6
Lenoir Nicolas <nicolas.lenoir52@sfr.fr>
2021-12-30 16:00:43 +0100

    add routes

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.use('/api/stuff', stuffRoutes);
+app.use('/api/sauces', stuffRoutes);
 app.use('/api/auth', userRoutes);
 app.use('/images', express.static(path.join(__dirname, 'images')));
 module.exports = app;
