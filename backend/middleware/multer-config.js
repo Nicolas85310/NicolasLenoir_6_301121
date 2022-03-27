5fdddab5d2f56078c773b8945f345056c430be88
Lenoir Nicolas <nicolas.lenoir52@sfr.fr>
2022-03-27 19:43:56 +0200

    change multer

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -2,7 +2,7 @@ const multer = require('multer');
 
 const MIME_TYPES = {
   'image/jpg': 'jpg',
-  'image/jpeg': 'jpg',
+  'image/jpeg': 'jpeg',
   'image/png': 'png'
 };
 
@@ -11,8 +11,8 @@ const storage = multer.diskStorage({
     callback(null, 'images');
   },
   filename: (req, file, callback) => {
-    const name = file.originalname.split(' ').join('_');
     const extension = MIME_TYPES[file.mimetype];
+    const name = file.originalname.split(' ').join('_');
     callback(null, name + Date.now() + '.' + extension);
   }
 });
